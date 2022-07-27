354567753484937419f5006f0c51e3a47b6974c5
hamlanreh <oluwabihammed49@gmail.com>
2022-07-27 12:22:22 +0100

    Update server port

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'http://localhost:5000'],
+    origin: 'http://localhost:3000',
     credentials: true,
   })
 );
