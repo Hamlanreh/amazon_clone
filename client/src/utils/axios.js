c80fd9ca9feb3019b7ff8caed99bbbba93350615
hamlanreh <oluwabihammed49@gmail.com>
2022-07-28 21:09:44 +0100

    Update axios

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://amazon-clone-mern-dev.herokuapp.com/api/v1/',
+  baseURL: '/api/v1/',
   withCredentials: true,
   //   headers: {
   //     Authorization: '<Your Auth Token>',
